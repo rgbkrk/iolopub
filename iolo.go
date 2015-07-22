84beeb013be05bc09123eb8f5f5108b21d2064d0
Kyle Kelley <rgbkrk@gmail.com>
2015-07-21 23:21:35 -0700

    IOLO

---
+++ iolo.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	juno "github.com/rgbkrk/juno"
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		log.Fatalln("Need a connection file.")
+	}
+
+	// Expects a runtime kernel-*.json
+	connInfo, err := juno.OpenConnectionFile(flag.Arg(0))
+
+	if err != nil {
+		fmt.Errorf("%v\n", err)
+		os.Exit(1)
+	}
+
+	iopub, err := juno.NewIOPubSocket(connInfo, "")
+
+	if err != nil {
+		fmt.Errorf("Couldn't start the iopub socket: %v", err)
+	}
+
+	defer iopub.Close()
+
+	for {
+		message, err := iopub.ReadMessage()
+
+		if err != nil {
+			fmt.Errorf("%v\n", err)
+			continue
+		}
+
+		//c, err := json.Marshal(message)
+		//fmt.Println(string(c))
+
+		_, ok := message.Content["data"]
+
+		if !ok {
+			continue
+		}
+
+		mimeBundle := (message.Content["data"]).(map[string]interface{})
+
+		// There and back again
+		b, err := json.Marshal(mimeBundle)
+		fmt.Println(string(b))
+
+	}
+
+}
